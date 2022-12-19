b7cf6b8749ffa672cf34863c51dda7bf362439c6
Jason Watmore <cornflourblue@hotmail.com>
2022-12-19 13:15:58 +1100

    minor refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 ﻿import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({ selector: 'app-root', templateUrl: 'app.component.html' })
 export class AppComponent {
-    model: any = {};
+    onSubmit(f: NgForm) {
+        // stop here if form is invalid
+        if (f.invalid) {
+            return;
+        }
 
-    onSubmit() {
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(f.value, null, 4));
     }
 }
